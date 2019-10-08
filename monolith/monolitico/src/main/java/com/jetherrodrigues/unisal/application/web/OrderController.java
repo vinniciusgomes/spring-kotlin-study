ced510ae19ca7f5efa9f756e9b43990ecca5fc19
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-08 17:33:58 -0300

    removed the transactional annotations

--- monolith/monolitico/src/main/java/com/jetherrodrigues/unisal/application/web/OrderController.java
+++ monolith/monolitico/src/main/java/com/jetherrodrigues/unisal/application/web/OrderController.java
@@ -24,7 +24,7 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
-    @Transactional
+    // @Transactional
     public ResponseEntity<?> create(@RequestBody @Valid final OrderRequest request,
                                     final UriComponentsBuilder uriBuilder) {
 
