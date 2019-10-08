059d6c1de071c166de0c7c7da549ab76a33c553e
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-08 17:35:57 -0300

    added order item repository

--- monolith/monolitico/src/main/java/com/jetherrodrigues/unisal/domain/order/service/OrderService.java
+++ monolith/monolitico/src/main/java/com/jetherrodrigues/unisal/domain/order/service/OrderService.java
@@ -1,20 +1,20 @@
 package com.jetherrodrigues.unisal.domain.order.service;
 
 import com.jetherrodrigues.unisal.domain.order.Order;
+import com.jetherrodrigues.unisal.domain.order.repository.OrderItemRepository;
 import com.jetherrodrigues.unisal.domain.order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderRepository repository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
-    @Transactional
-    public Order save(Order order) {
+    public Order save(final Order order) {
         return repository.save(order);
     }
 
